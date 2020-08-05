d43c96b9a60a08493053fd104d82243dce7e8d36
Bruno Eduardo <bruno97br@gmail.com>
2020-08-05 14:18:27 -0300

    Hello World of accelerometer dump

--- AccLogger/App.js
+++ AccLogger/App.js
@@ -1,21 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Accelerometer } from 'expo-sensors';
 
 export default function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    _toggle();
+  }, []);
+
+  useEffect(() => {
+    return () => {
+      _unsubscribe();
+    };
+  }, []);
+
+  const _toggle = () => {
+    if (this._subscription) {
+      _unsubscribe();
+    } else {
+      _subscribe();
+    }
+  };
+
+  const _slow = () => {
+    Accelerometer.setUpdateInterval(1000);
+  };
+
+  const _fast = () => {
+    Accelerometer.setUpdateInterval(1);
+  };
+
+  const _subscribe = () => {
+    this._subscription = Accelerometer.addListener(accelerometerData => {
+      setData(accelerometerData);
+    });
+  };
+
+  const _unsubscribe = () => {
+    this._subscription && this._subscription.remove();
+    this._subscription = null;
+  };
+
+  let { x, y, z } = data;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Accelerometer: (in Gs where 1 G = 9.81 m s^-2)</Text>
+      <Text>
+        x: {round(x)} y: {round(y)} z: {round(z)}
+      </Text>
+      <View>
+        <TouchableOpacity onPress={_toggle}>
+          <Text>Toggle</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={_slow}>
+          <Text>Slow</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={_fast}>
+          <Text>Fast</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function round(n) {
+  if (!n) {
+    return 0;
+  }
+
+  return Math.floor(n * 1000000) / 1000000;
+}
+
