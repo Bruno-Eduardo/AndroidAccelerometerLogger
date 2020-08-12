92707f5c25d9822476ce041ca156538605966fa3
Bruno Eduardo <bruno97br@gmail.com>
2020-08-12 23:47:50 +0000

    todo itens for future

--- backend/src/controllers/RawDataReceiverController.js
+++ backend/src/controllers/RawDataReceiverController.js
@@ -1,7 +1,16 @@
+//const connection = require('../database/connection');
+
 module.exports={
     async create(request, response) {
         console.log(request.body)
         const {time, x, y, z} = request.body;
+
+        // TODO: use a database
+        //await connection('accel').insert({
+        //    x,
+        //    y,
+        //    z,
+        //});
 
         fs = require('fs');
         fs.writeFile('helloworld.txt',
